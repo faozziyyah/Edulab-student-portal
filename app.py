04e679378b3842b7d9d2b8dc338de3fdae883e6a
Your name <omowunmidaud1@gmail.com>
2022-09-01 05:34:50 +0100

    twearked app for heroku hosting

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 app.secret_key = 'secret'
 def get_db_connection():
-  conn = psycopg2.connect("dbname=portal user=postgres password=opeyemi2001 host=localhost")
+  #conn = psycopg2.connect("dbname=portal user=postgres password=opeyemi2001 host=localhost")
+  conn = psycopg2.connect("postgres://lrqtcehzeiejyl:48466d99f2102da9054a975354b4322becfede3381f3777ffcc0a649d7c54009@ec2-35-168-122-84.compute-1.amazonaws.com:5432/dbfh4rod9jc9tg")
   return conn
 
 @app.route('/')
